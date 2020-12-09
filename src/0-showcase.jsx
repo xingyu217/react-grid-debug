350d35325435f54d742699198b28d3525639ac2b
starain <xingyu217@163.com>
2020-12-09 22:39:15 +0800

    1

---
+++ src/0-showcase.jsx
@@ -0,0 +1,144 @@
+// @flow
+import React from "react";
+import _ from "lodash";
+import Responsive from './lib/ResponsiveReactGridLayout';
+import WidthProvider from './lib/components/WidthProvider';
+import type {CompactType, Layout} from './lib/utils';
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
+
+type Props = {|
+  className: string,
+  cols: {[string]: number},
+  
+  rowHeight: number,
+|};
+type State = {|
+  currentBreakpoint: string,
+  compactType: CompactType,
+  mounted: boolean,
+  layouts: {[string]: Layout}
+|};
+
+export default class ShowcaseLayout extends React.Component<Props, State> {
+  static defaultProps = {
+    className: "layout",
+    rowHeight: 30,
+    cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
+  };
+
+  state = {
+    currentBreakpoint: "lg",
+    compactType: "vertical",
+    mounted: false,
+    layouts: { lg: generateLayout() }
+  };
+
+  componentDidMount() {
+    this.setState({ mounted: true });
+  }
+
+  generateDOM() {
+    return _.map(this.state.layouts.lg, function(l, i) {
+      return (
+        <div key={i} className={l.static ? "static" : ""}>
+          {l.static ? (
+            <span
+              className="text"
+              title="This item is static and cannot be removed or resized."
+            >
+              Static - {i}
+            </span>
+          ) : (
+            <span className="text">{i}</span>
+          )}
+        </div>
+      );
+    });
+  }
+
+  onBreakpointChange = (breakpoint: string) => {
+    this.setState({
+      currentBreakpoint: breakpoint
+    });
+  };
+
+  onCompactTypeChange = () => {
+    const { compactType: oldCompactType } = this.state;
+    const compactType =
+      oldCompactType === "horizontal"
+        ? "vertical"
+        : oldCompactType === "vertical"
+        ? null
+        : "horizontal";
+    this.setState({ compactType });
+  };
+
+  onLayoutChange = (layout: Layout, layouts: {[string]: Layout}) => {
+    console.log("dd");
+    //this.props.onLayoutChange(layout, layouts);
+  };
+
+  onNewLayout = () => {
+    this.setState({
+      layouts: { lg: generateLayout() }
+    });
+  };
+
+  onDrop = (elemParams: Object) => {
+    alert(`Element parameters: ${JSON.stringify(elemParams)}`);
+  };
+
+  render() {
+    // eslint-disable-next-line no-unused-vars
+    return (
+      <div>
+        <div>
+          Current Breakpoint: {this.state.currentBreakpoint} (
+          {this.props.cols[this.state.currentBreakpoint]} columns)
+        </div>
+        <div>
+          Compaction type:{" "}
+          {_.capitalize(this.state.compactType) || "No Compaction"}
+        </div>
+        <button onClick={this.onNewLayout}>Generate New Layout</button>
+        <button onClick={this.onCompactTypeChange}>
+          Change Compaction Type
+        </button>
+        <ResponsiveReactGridLayout
+          {...this.props}
+          layouts={this.state.layouts}
+          onBreakpointChange={this.onBreakpointChange}
+          onLayoutChange={this.onLayoutChange}
+          onDrop={this.onDrop}
+          // WidthProvider option
+          measureBeforeMount={false}
+          // I like to have it animate on mount. If you don't, delete `useCSSTransforms` (it's default `true`)
+          // and set `measureBeforeMount={true}`.
+          useCSSTransforms={this.state.mounted}
+          compactType={this.state.compactType}
+          preventCollision={!this.state.compactType}
+        >
+          {this.generateDOM()}
+        </ResponsiveReactGridLayout>
+      </div>
+    );
+  }
+}
+
+function generateLayout() {
+  return _.map(_.range(0, 25), function(item, i) {
+    var y = Math.ceil(Math.random() * 4) + 1;
+    return {
+      x: Math.round(Math.random() * 5) * 2,
+      y: Math.floor(i / 6) * y,
+      w: 2,
+      h: y,
+      i: i.toString(),
+      static: Math.random() < 0.05
+    };
+  });
+}
+
+// if (process.env.STATIC_EXAMPLES === true) {
+//   import("../test-hook.jsx").then(fn => fn.default(ShowcaseLayout));
+// }
---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import ShowcaseLayout from './0-showcase';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    <ShowcaseLayout
+    className="Layout"
+    cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+    rowHeight={100}
+    />
+    </>
+  );
+}
+
+export default App;
